ff260830b7c351e3925a1e1e4084af57384852c4
Harold AO <>
2021-05-30 18:39:40 +0000

    Setup scene + create blue cube

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-console.log('Hello Three.js');
-console.log(THREE);
+// Scene
+const scene = new THREE.Scene();
 
+// Blue Cube
+const geometry =new THREE.BoxGeometry(1, 1, 1);
+      material = new THREE.MeshBasicMaterial({color: 'blue'});
+      mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
